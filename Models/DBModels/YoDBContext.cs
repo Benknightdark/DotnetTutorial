41b221829b3f2b4ccdf7c745a2c1a99323d5f3da
Ben Kung <ben_kung@infofab.com>
2019-09-23 11:13:16 +0800

    OK

--- Models/DBModels/YoDBContext.cs
+++ Models/DBModels/YoDBContext.cs
@@ -2,39 +2,48 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace DotnetTutorial.Models.DBModels {
-    public partial class YoDBContext : DbContext {
+namespace DotnetTutorial.Models.DBModels
+{
+    public partial class YoDBContext : DbContext
+    {
+        public YoDBContext()
+        {
+        }
 
-        public static long InstanceCount;
-        public YoDBContext (DbContextOptions<YoDBContext> options) : base (options) => System.Threading.Interlocked.Increment (ref InstanceCount);
+        public YoDBContext(DbContextOptions<YoDBContext> options)
+            : base(options)
+        {
+        }
 
         public virtual DbSet<user> user { get; set; }
 
-        //         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //         {
-        //             if (!optionsBuilder.IsConfigured)
-        //             {
-        // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-        //                 optionsBuilder.UseSqlServer("Server=.,5269  ;Database=yo;user id=sa;password=yourStrong(!)Password");
-        //             }
-        //         }
-
-        protected override void OnModelCreating (ModelBuilder modelBuilder) {
-            modelBuilder.Entity<user> (entity => {
-                entity.Property (e => e.Id).ValueGeneratedNever ();
-
-                entity.Property (e => e.name)
-                    .IsRequired ()
-                    .HasMaxLength (50);
-
-                entity.Property (e => e.phone)
-                    .IsRequired ()
-                    .HasMaxLength (50);
-            });
-
-            OnModelCreatingPartial (modelBuilder);
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.,5269  ;Database=yo;user id=sa;password=yourStrong(!)Password");
+            }
         }
 
-        partial void OnModelCreatingPartial (ModelBuilder modelBuilder);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<user>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.phone)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
